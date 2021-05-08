d72b40b9f00655a0de1a7392158604e7713bf54d
paulinhalopessantos <linha_vitoria@hotmail.com>
2021-05-08 10:01:35 -0300

    estrutura inicial

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,3 +1,24 @@
-const App = () => <h1>Portfólio</h1>
+import React from 'react'
+import {
+  MainContainer,
+  AvatarTitleContainer,
+  Avatar,
+  Title,
+  MenuAboutMeContainer,
+  LateralMenu,
+  AboutMe
+} from './styled'
+const App = () => (
+  <MainContainer>
+    <AvatarTitleContainer>
+      <Avatar>Avatar</Avatar>
+      <Title>Title</Title>
+    </AvatarTitleContainer>
+    <MenuAboutMeContainer>
+      <LateralMenu>Menu</LateralMenu>
+      <AboutMe>Sobre mim</AboutMe>
+    </MenuAboutMeContainer>
+  </MainContainer>
+)
 
 export default App
---
+++ src/pages/styled.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+export const MainContainer = styled.div`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  box-sizing: border-box;
+  flex-direction: column;
+`
+export const AvatarTitleContainer = styled.div`
+  display: flex;
+  height: 30vh;
+`
+export const Avatar = styled.div`
+  display: flex;
+  background-color: blue;
+  width: 230px;
+`
+export const Title = styled.div`
+  display: flex;
+  background-color: yellow;
+  flex-grow: 1;
+`
+
+export const MenuAboutMeContainer = styled.div`
+  display: flex;
+  height: 70vh;
+`
+
+export const LateralMenu = styled.div`
+  display: flex;
+  background-color: green;
+  width: 230px;
+`
+
+export const AboutMe = styled.div`
+  display: flex;
+  background-color: blue;
+  flex-grow: 1;
+`
