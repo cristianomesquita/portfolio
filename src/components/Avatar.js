5b263d8d38ab0a88f17ef2d9d137612e65b8360d
Paula Lopes <77751963+paula-lopes@users.noreply.github.com>
2021-06-19 20:24:39 -0300

    Update src/components/Avatar.js
    Co-authored-by: Cristiano Mesquita <contact@cristianomesquita.com>

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Avatar = props => {
+const Avatar = ({ alt, height, src, width }) => 
+   <img src={src} alt={alt} width={width} height={height} />
   return <img src={props.src} alt={props.alt} width={props.width} height={props.height} />
 }
 
