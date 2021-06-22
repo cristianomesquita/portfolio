918a752d146b21c4feaec7ebc0bb26b6788e4512
paula-lopes <paulalopessantos28@gmail.com>
2021-06-22 07:54:53 -0300

    solving problems

---
+++ src/components/Accordion/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import vectorMinus from 'assets/vectorShow'
+import vectorMore from 'assets/vectorHidden'
+import { AccordionContainer, Title, Content } from './styles'
+
+const Accordion = ({ content, title }) => (
+  <AccordionContainer>
+    <Title> {title}</Title>
+    <Content> {content}</Content>
+  </AccordionContainer>
+)
+
+export default Accordion
---
+++ src/components/Accordion/styles.js
@@ -0,0 +1,7 @@
+export const AccordionContainer = styled.div`
+  display: flex;
+`
+
+export const Title = styled.h1``
+
+export const Content = styled.p``
